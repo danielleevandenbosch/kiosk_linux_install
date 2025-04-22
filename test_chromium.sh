ac14bd5cfb2ac1c0dd00ccb719d373109b11c235
Daniel Van Den Bosch <danielleevandenbosch@gmail.com>
2025-04-22 14:57:15 -0400

    Update test_chromium.sh

--- test_chromium.sh
+++ test_chromium.sh
@@ -2,4 +2,7 @@ SOCKET=$(find /run/user/1001 -maxdepth 1 -type s -name 'wayland-*' | head -n1)
 export XDG_RUNTIME_DIR=/run/user/1001
 export WAYLAND_DISPLAY=$(basename "$SOCKET")
 
-sudo -u gui dbus-run-session -- flatpak run org.chromium.Chromium --ozone-platform=wayland
+sudo -u gui \
+  XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+  dbus-run-session -- flatpak run org.chromium.Chromium --ozone-platform=wayland
