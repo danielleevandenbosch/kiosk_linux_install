2fd3015b41cc4c744031f6255dce13049d3cc852
Daniel Van Den Bosch <danielleevandenbosch@gmail.com>
2025-05-06 10:27:37 -0400

    Update toggle_keyboard_button.sh

--- x11_with_keyboard/dotfiles/toggle_keyboard_button.sh
+++ x11_with_keyboard/dotfiles/toggle_keyboard_button.sh
@@ -3,11 +3,12 @@
 SCREEN_W=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d 'x' -f1)
 SCREEN_H=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d 'x' -f2)
 KBD_H=$(( SCREEN_H / 3 ))
-BROWSER_H=$(( SCREEN_H - KBD_H ))
+TOGGLE_H=40
+BROWSER_H=$(( SCREEN_H - KBD_H - TOGGLE_H ))
 
 if pgrep onboard >/dev/null; then
   pkill onboard
-  wmctrl -r :ACTIVE: -e 0,0,0,$SCREEN_W,$SCREEN_H
+  wmctrl -r :ACTIVE: -e 0,0,$TOGGLE_H,$SCREEN_W,$(( SCREEN_H - TOGGLE_H ))
 else
   onboard &
   sleep 2
